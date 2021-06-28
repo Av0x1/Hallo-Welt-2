91da7aa4db9e5a83737e8c1586cedc523e504c4d
seeker1366@gmail.com <86572524+Av0x1@users.noreply.github.com>
2021-06-28 19:37:59 +0200

    Projektdateien hinzufügen.

---
+++ Hallo Welt 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hallo_Welt_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund dog = new Hund();
+
+            dog.Name = "Coco";
+            {
+                Console.WriteLine("Der Hund heißt " + dog.Name + ".");
+            }
+            dog.Geschlecht = "Rüde";
+            {
+                Console.WriteLine(dog.Name + " ist ein " + dog.Geschlecht + ".");
+            }
+            dog.Bellen();
+            dog.Bewegen();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Tier
+    {
+        //Eigenschaften
+
+        public string Geschlecht { get; set; }
+
+        //Methoden
+
+        public void Bewegen()
+        {
+            Console.WriteLine("Bewegen...");
+        }
+
+    }
+
+    class Hund : Tier
+    {
+        //Eigenschaften
+
+        public string Name { get; set; }
+        public string Rasse { get; set; }
+
+        //Methoden
+
+        public void Bellen()
+        {
+            Console.WriteLine("Bellen...");
+        }
+        public void Schlecken()
+        {
+            Console.WriteLine("Schlecken...");
+        }
+    }
+}
