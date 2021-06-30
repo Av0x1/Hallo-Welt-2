f94fdc6846d5b97fac82e3eb28352ff86d4acc51
seeker1366@gmail.com <86572524+Av0x1@users.noreply.github.com>
2021-06-30 13:10:15 +0200

    Hundeobjekte bearbeitet

--- Hallo Welt 2/Program.cs
+++ Hallo Welt 2/Program.cs
@@ -12,25 +12,25 @@ namespace Hallo_Welt_2
         {
             Console.Title = "Haustierprogramm";
 
-            Console.WriteLine("Guten Tag, ich bin das Haustierprogramm. Mit wem kommuniziere ich?");
+            Console.WriteLine("Guten Tag, ich bin das Haustierprogramm.\nMit wem kommuniziere ich?");
 
             string userName = Console.ReadLine();
-            Console.WriteLine("\n" + userName + "? Das ist aber ein schöner Name. Bist du denn männlich oder weiblich?");
+            Console.WriteLine("\n" + userName + "? Das ist aber ein schöner Name.\nBist du denn männlich oder weiblich oder ein anderes Geschlecht??");
             string userGeschlecht = Console.ReadLine();
 
             switch (userGeschlecht)
             {
                 case "Männlich":
-                        Console.WriteLine("\nDu bist also ein Mann? Sehr interessant. Wie alt bist du denn?");
                         Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("\nDu bist also ein Mann? Sehr interessant.\nWie alt bist du denn?");
                     break;
                 case "Weiblich":
-                        Console.WriteLine("\nDu bist also eine Frau? Sehr interessant. Wie alt bist du denn?");
                         Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nDu bist also eine Frau? Sehr interessant.\nWie alt bist du denn?");
                     break;
                 default:
-                        Console.WriteLine("\nDu bist also Divers? Sehr interessant. Wie alt bist du denn?");
                         Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("\nDu bist also Divers? Sehr interessant.\nWie alt bist du denn?");
                     break;
             }
             string reqAlter = Console.ReadLine();
@@ -38,85 +38,28 @@ namespace Hallo_Welt_2
 
             if(numAlter < 18)
             {
-                Console.WriteLine("Es tut mir leid. Du bist zu jung um dieses Programm nutzen zu dürfen. Dieses Programm ist ab 18 Jahren.\n");
+                Console.WriteLine("\nEs tut mir leid. Du bist zu jung um dieses Programm nutzen zu dürfen. Dieses Programm ist ab 18 Jahren.\n");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
             if(numAlter >= 18)
             {
-                Console.WriteLine("Du bist alt genug. Das Haustierprogramm ermöglicht es dir ein beliebiges Haustier zu konfigurieren.\nEntscheide dich für eins dieser Tiere: Hund, Katze, Hamster oder Schildkröte./n");
+                Console.WriteLine("\nDu bist alt genug. Das Haustierprogramm ermöglicht es dir ein beliebiges Haustier zu konfigurieren.\nEntscheide dich für eins dieser Tiere:\n- Hund\n- Katze\n- Schildkröte\n");
             }
             string userInput = Console.ReadLine();
 
-            //Code Auswahl: Hund
-
             switch (userInput)
             {
+                //Code Auswahl: Hund
                 case "Hund":
                     {
-                        Hund dog2 = new Hund();
-
-                        dog2.Name = "Coco";
-                        {
-                            Console.WriteLine("Der Hund heißt " + dog2.Name + ".");
-                        }
-                        dog2.Geschlecht = "Rüde";
-                        {
-                            if (dog2.Geschlecht == "Rüde")
-                            {
-                                Console.WriteLine(dog2.Name + " ist ein " + dog2.Geschlecht + ".");
-                            }
-                            else
-                            {
-                                Console.WriteLine(dog2.Name + " ist ein Weibchen.");
-                            }
-                        }
-                        dog2.Rasse = "Husky";
-                        {
-                            switch(dog2.Rasse)
-                            {
-                                case "Golden Retriever":
-                                    dog2.Fellfarbe = "Gold";
-                                    break;
-                                case "Husky":
-                                    dog2.Fellfarbe = "Weiß und Schwarz";
-                                    break;
-                                case "Dackel":
-                                    dog2.Fellfarbe = "Braun";
-                                    break;
-                                case "Rottweiler":
-                                    dog2.Fellfarbe = "Schwarz";
-                                    break;
-                                case "Chihuahua":
-                                    dog2.Fellfarbe = "Hellbraun";
-                                    break;
-                                default:
-                                        Console.WriteLine("Ungültige Rasse!");
-                                    break;
-                            }
-                            Console.WriteLine(dog2.Name + " ist ein " + dog2.Rasse + " und das Fell ist " + dog2.Fellfarbe + ".");
-                        }
-                        Console.ReadKey();
-
-                        Console.WriteLine("Möchtest du den nächsten Hund kennenlernen? Ja oder Nein?");
-                        string nextDog = Console.ReadLine();
-
-                        switch(nextDog)
-                        {
-                            case "Nein":
-                                Console.WriteLine("Sehr schade. Dann bis zum nächsten Mal! Schönen Tag!");
-                                Console.ReadKey();
-                                break;
-                            case "Ja":
-                                Console.WriteLine("Sehr schön, dann ist hier der nächste Hund:");
-                                break;
-                        }
+                        //Vorstellung Hund 1
 
                         Hund dog1 = new Hund();
 
                         dog1.Name = "Coco";
                         {
-                            Console.WriteLine("Der Hund heißt " + dog1.Name + ".");
+                            Console.WriteLine("\nDer Hund heißt " + dog1.Name + ".");
                         }
                         dog1.Geschlecht = "Rüde";
                         {
@@ -124,14 +67,14 @@ namespace Hallo_Welt_2
                             {
                                 Console.WriteLine(dog1.Name + " ist ein " + dog1.Geschlecht + ".");
                             }
-                            else
+                            if (dog1.Geschlecht == "Hündin")
                             {
                                 Console.WriteLine(dog1.Name + " ist ein Weibchen.");
                             }
                         }
                         dog1.Rasse = "Husky";
                         {
-                            switch (dog1.Rasse)
+                            switch(dog1.Rasse)
                             {
                                 case "Golden Retriever":
                                     dog1.Fellfarbe = "Gold";
@@ -149,12 +92,152 @@ namespace Hallo_Welt_2
                                     dog1.Fellfarbe = "Hellbraun";
                                     break;
                                 default:
-                                    Console.WriteLine("Ungültige Rasse!");
+                                        Console.WriteLine("Ungültige Rasse!");
                                     break;
                             }
                             Console.WriteLine(dog1.Name + " ist ein " + dog1.Rasse + " und das Fell ist " + dog1.Fellfarbe + ".");
                         }
                         Console.ReadKey();
+
+                        Console.WriteLine("\nMöchtest du den nächsten Hund kennenlernen? Ja oder Nein?");
+                        string nextDog = Console.ReadLine();
+
+                        switch(nextDog)
+                        {
+                            case "Nein":
+                                Console.WriteLine("Sehr schade. Dann bis zum nächsten Mal! Schönen Tag!");
+                                Console.ReadKey();
+                                Environment.Exit(0);
+                                break;
+                            case "Ja":
+                                Console.WriteLine("Sehr schön, dann ist hier der nächste Hund:");
+                                break;
+                        }
+
+                        //Vorstellung Hund 2
+
+                        Hund dog2 = new Hund();
+
+                        dog2.Name = "Rudi";
+                        {
+                            Console.WriteLine("Der Hund heißt " + dog2.Name + ".");
+                        }
+                        dog2.Geschlecht = "Hündin";
+                        {
+                            if (dog2.Geschlecht == "Rüde")
+                            {
+                                Console.WriteLine(dog2.Name + " ist ein " + dog2.Geschlecht + ".");
+                            }
+                            if (dog2.Geschlecht == "Hündin")
+                            {
+                                Console.WriteLine(dog2.Name + " ist ein Weibchen.");
+                            }
+                        }
+                        dog2.Rasse = "Husky";
+                        {
+                            switch (dog2.Rasse)
+                            {
+                                case "Golden Retriever":
+                                    dog2.Fellfarbe = "Gold";
+                                    break;
+                                case "Husky":
+                                    dog2.Fellfarbe = "Weiß und Schwarz";
+                                    break;
+                                case "Dackel":
+                                    dog2.Fellfarbe = "Braun";
+                                    break;
+                                case "Rottweiler":
+                                    dog2.Fellfarbe = "Schwarz";
+                                    break;
+                                case "Chihuahua":
+                                    dog2.Fellfarbe = "Hellbraun";
+                                    break;
+                                default:
+                                    Console.WriteLine("Ungültige Rasse!");
+                                    break;
+                            }
+                            Console.WriteLine(dog2.Name + " ist ein " + dog2.Rasse + " und das Fell ist " + dog2.Fellfarbe + ".");
+                        }
+                        Console.ReadKey();
+
+                        Console.WriteLine("\nMöchtest du den nächsten Hund kennenlernen? Ja oder Nein?");
+                        string nextDog1 = Console.ReadLine();
+
+                        switch (nextDog1)
+                        {
+                            case "Nein":
+                                Console.WriteLine("Sehr schade. Dann bis zum nächsten Mal! Schönen Tag!");
+                                Console.ReadKey();
+                                Environment.Exit(0);
+                                break;
+                            case "Ja":
+                                Console.WriteLine("Sehr schön, dann ist hier der nächste Hund:");
+                                break;
+                        }
+
+                        // Vorstellung Hund 3
+
+                        Hund dog3 = new Hund();
+
+                        dog3.Name = "Rudi";
+                        {
+                            Console.WriteLine("Der Hund heißt " + dog3.Name + ".");
+                        }
+                        dog3.Geschlecht = "Hündin";
+                        {
+                            if (dog3.Geschlecht == "Rüde")
+                            {
+                                Console.WriteLine(dog3.Name + " ist ein " + dog3.Geschlecht + ".");
+                            }
+                            if (dog3.Geschlecht == "Hündin")
+                            {
+                                Console.WriteLine(dog3.Name + " ist ein Weibchen.");
+                            }
+                        }
+                        dog3.Rasse = "Husky";
+                        {
+                            switch (dog3.Rasse)
+                            {
+                                case "Golden Retriever":
+                                    dog3.Fellfarbe = "Gold";
+                                    break;
+                                case "Husky":
+                                    dog3.Fellfarbe = "Weiß und Schwarz";
+                                    break;
+                                case "Dackel":
+                                    dog3.Fellfarbe = "Braun";
+                                    break;
+                                case "Rottweiler":
+                                    dog3.Fellfarbe = "Schwarz";
+                                    break;
+                                case "Chihuahua":
+                                    dog3.Fellfarbe = "Hellbraun";
+                                    break;
+                                default:
+                                    Console.WriteLine("Ungültige Rasse!");
+                                    break;
+                            }
+                            Console.WriteLine(dog3.Name + " ist ein " + dog3.Rasse + " und das Fell ist " + dog3.Fellfarbe + ".");
+                        }
+                        Console.ReadKey();
+
+                        Console.WriteLine("\nMöchtest du den nächsten Hund kennenlernen? Ja oder Nein?");
+                        string nextDog2 = Console.ReadLine();
+
+                        switch (nextDog1)
+                        {
+                            case "Nein":
+                                Console.WriteLine("Sehr schade. Dann bis zum nächsten Mal! Schönen Tag!");
+                                Console.ReadKey();
+                                Environment.Exit(0);
+                                break;
+                            case "Ja":
+                                Console.WriteLine("Sehr schön, dann ist hier der nächste Hund:");
+                                break;
+                        }
+
+
+
                     }
                     break;
             }
